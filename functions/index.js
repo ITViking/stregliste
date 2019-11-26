6f43cc7a1675b67185c447ec87a85f4c56eef59c
ITViking <philip.dein@gmail.com>
2019-11-26 21:41:37 +0100

    add root page and allow for project restart

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -16,7 +17,24 @@ exports.setRegularUserRights = functions.auth.user().onCreate((user) => {
 
     return admin.auth().setCustomUserClaims(user.uid, customClaims)
         .then(() => {
-        //     admin.firestore().collection("session").doc(user.uid).set({ refreshTime: new Date().getTime() });
+            //     admin.firestore().collection("session").doc(user.uid).set({ refreshTime: new Date().getTime() });
         })
         .catch(console.error);
+});
+
+exports.restart_project = functions.https.onCall((data, context) => {
+    console.log("recieved data: ", data);
+    let users = data.users;
+
+    if(!users) return { message: "no users recieved" };
+
+    users.map(user => {
+        try {
+            admin.auth().deleteUser(user.uid)
+        } catch(error) {
+            console.error("failed to delete user: ", user);
+        }
+
+        return { message: "users were deleted from auth" };
+    });
 });
--- pages/rootAdministration.vue
+++ pages/rootAdministration.vue
@@ -1,8 +1,102 @@
 <template>
-    <v-layout column justify-center align-center>
+  <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <v-container>
-          </v-container>
+        <v-data-table
+          :headers="headers"
+          disable-pagination
+          :items="users"
+          hide-default-footer
+          class="elevation-1"
+        >
+          <template v-slot:body="{ items }">
+            <tbody>
+              <tr v-for="item in items" :key="item.name" @click="editOffer(item)">
+                <td>{{ item.name }}</td>
+                <td>{{ item.role }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-data-table>
+        <v-btn @click="restartProject" color="error" large class="mt-12">Restart project</v-btn>
+      </v-container>
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import axios from "axios";
+import { firestore } from "../firebaseSetup";
+
+export default {
+  data() {
+    return {
+      users: [],
+      headers: [
+        { text: "Navn", value: "name" },
+        { text: "Rolle", value: "bruger" }
+      ]
+    };
+  },
+  methods: {
+    listUsers() {
+      firestore.collection("users").onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type == "added") {
+            let user = {
+              uid: change.doc.id,
+              ...change.doc.data()
+            };
+            this.users.push(user);
+          }
+        });
+        console.log(this.users);
+      });
+    },
+    async restartProject() {
+      //This function sole purpose is to allow for starting over due to the restrictive nature of firebase.
+      let payload = {
+        data: {
+          users: this.users
+        }
+      };
+
+      let headers = {
+        "Content-type": "application/json"
+      };
+
+      let response;
+      try {
+        response = await axios.post(
+          "https://us-central1-stregliste-79a6d.cloudfunctions.net/restart_project",
+          payload,
+          {
+            headers
+          }
+        );
+      } catch (error) {
+        console.error("faild to restart project: ", error);
+        return;
+      }
+      this.deleteUsersCollection();
+      console.log("projected restarted :", response);
+    },
+    deleteUsersCollection() {
+      this.users.map(user => {
+        try {
+          firestore
+            .collection("users")
+            .doc(user.uid)
+            .delete();
+        } catch (error) {
+          console.error("failed to delete user from collection:", error);
+          return;
+        }
+      });
+    }
+  },
+  created() {
+    this.listUsers();
+  }
+};
+</script>
