a4819be89195cf163c6a7e02024fb64904ece1b3
ITViking <philip.dein@gmail.com>
2019-11-26 18:02:04 +0100

    function to make a user admin

--- functions/index.js
+++ functions/index.js
@@ -10,13 +10,35 @@ admin.initializeApp();
 // });
 
 exports.setRegularUserRights = functions.auth.user().onCreate((user) => {
-    let customClaims = {
+    let basicUserClaim = {
         admin: false
     };
 
-    return admin.auth().setCustomUserClaims(user.uid, customClaims)
+    return admin.auth().setCustomUserClaims(user.uid, basicUserClaim)
         .then(() => {
-        //     admin.firestore().collection("session").doc(user.uid).set({ refreshTime: new Date().getTime() });
+            //     admin.firestore().collection("session").doc(user.uid).set({ refreshTime: new Date().getTime() });
         })
         .catch(console.error);
+});
+
+exports.set_admin = functions.https.onCall((data, context) => {
+    let user = data.user;
+
+    let adminClaim = {
+        admin: true
+    };
+
+    try {
+        admin.auth().setCustomUserClaims(user.uid , adminClaim);
+    } catch (error) {
+        console.error(`failed to give ${user.name} an admin role: `, error);
+
+        return {
+            message: `failed to give ${user.name} an admin role: `, error
+        }
+    }
+
+    return {
+        message: `${user.name} is now an admin`
+    };
 });
