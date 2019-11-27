d62499f090fdd7b7fd935ae39568592c1f593077
ITViking <philip.dein@gmail.com>
2019-11-28 00:52:35 +0100

    if user is logged in they are sent directly to the tap, if not they are sent to login

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,7 +1,7 @@
 
 export default function ({ store, redirect, route }) {
-
-    console.log("hit mw");
-    if(!store.state.user) return redirect("/");
-    return redirect();
+    if (!store.state.user) return redirect("/");
+    if (route.path == "/" && store.state.user) {
+        return redirect("/tap");
+    }
 };
