591edb544123330d8ee32f3a7f1f9d72e1f297db
ITViking <philip.dein@gmail.com>
2019-11-28 02:18:36 +0100

    add a cart to user and show their balance

--- pages/tap.vue
+++ pages/tap.vue
@@ -3,24 +3,36 @@
     <v-flex xs12 sm8 md6>
       <v-container>
         <h1 class="text-center mb-6">Velkommen til streglisten</h1>
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">Navn</th>
-                <th class="text-left text-center">10 kr</th>
-                <th class="text-left text-center">5 kr</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="user in users" :key="user.name">
-                <td>{{ user.name }}</td>
-                <td><v-btn>Tilføj</v-btn></td>
-                <td><v-btn>Tilføj</v-btn></td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
+        <v-card>
+          <v-data-table
+            :headers="headers"
+            disable-pagination
+            :items="users"
+            hide-default-footer
+            class="elevation-1"
+          >
+            <template v-slot:body="{ items }">
+              <tbody>
+                <tr v-for="user in items" :key="user.name">
+                  <td>{{ user.name }}</td>
+                  <td>
+                    <v-btn @click="addToUsersTap(user, 5)">
+                      <img src="coin.jpg" alt="coin" height="30p" width="30" />
+                    </v-btn>
+                  </td>
+                  <td>
+                    <v-btn @click="addToUsersTap(user, 10)">
+                      <img src="coin.jpg" alt="coin" height="30p" width="30" />
+                      <img src="coin.jpg" alt="coin" height="30p" width="30" />
+                    </v-btn>
+                  </td>
+                  <td v-if="user.cart != 0">{{ user.cart }}</td>
+                  <td>{{ user.balance }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-data-table>
+        </v-card>
       </v-container>
     </v-flex>
   </v-layout>
@@ -33,7 +45,12 @@ export default {
   data() {
     return {
       loading: false,
-      users: []
+      users: [],
+      headers: [
+        { text: "Navn", value: "name" },
+        { text: "Saldo", value: "balance" },
+      ],
+      width: 5,
     };
   },
   methods: {
@@ -41,9 +58,25 @@ export default {
       firestore.collection("users").onSnapshot(snapshot => {
         snapshot.docChanges().forEach(change => {
           if (change.type == "added") {
-            this.users.push(change.doc.data());
+            let user = {
+              uid: change.doc.id,
+              balance: change.doc.get("balance")
+                ? changes.doc.get("balance")
+                : 0,
+              cart: 0,
+              name: change.doc.get("name")
+            };
+
+            this.users.push(user);
           }
         });
+      });
+    },
+    addToUsersTap(userToAddTo, amount) {
+      this.users.map(user => {
+        if (user.uid == userToAddTo.uid) {
+          userToAddTo.cart += amount;
+        }
       });
     }
   },
