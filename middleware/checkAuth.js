700a2a9d26a217b730261b35700104e838e45726
ITViking <philip.dein@gmail.com>
2019-11-28 12:03:08 +0100

    handle routing based on user logged in better

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,7 +1,6 @@
 
 export default function ({ store, redirect, route }) {
-    if (!store.state.user) return redirect("/");
-    if (route.path == "/" && store.state.user) {
-        return redirect("/tap");
-    }
+    if (route.path == "/tap" && !store.state.user.email) return redirect("/");
+    if (route.path == "/" && store.state.user.email) return redirect("/tap");
+    if(route.path == "/root" && !store.state.user.isRoot) return redirect("/tap");
 };
