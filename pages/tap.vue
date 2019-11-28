2de3ef4c22c74c631335eecaacb7776239d618db
ITViking <philip.dein@gmail.com>
2019-11-28 02:49:08 +0100

    user is prompted if the added something to a users cart

--- pages/tap.vue
+++ pages/tap.vue
@@ -2,9 +2,10 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <v-container>
-        <h1 class="text-center mb-6">Velkommen til streglisten</h1>
+        <h1 class="text-center mb-6">Velkommen</h1>
         <v-card>
           <v-data-table
+            height="400"
             :headers="headers"
             disable-pagination
             :items="users"
@@ -16,14 +17,14 @@
                 <tr v-for="user in items" :key="user.name">
                   <td>{{ user.name }}</td>
                   <td>
-                    <v-btn @click="addToUsersTap(user, 5)">
-                      <img src="coin.jpg" alt="coin" height="30p" width="30" />
+                    <v-btn x-small @click="addToUsersTap(user, 5)">
+                      <img src="coin.jpg" alt="coin" height="25" width="25" />
                     </v-btn>
                   </td>
                   <td>
-                    <v-btn @click="addToUsersTap(user, 10)">
-                      <img src="coin.jpg" alt="coin" height="30p" width="30" />
-                      <img src="coin.jpg" alt="coin" height="30p" width="30" />
+                    <v-btn x-small @click="addToUsersTap(user, 10)">
+                      <img src="coin.jpg" alt="coin" height="25" width="25" />
+                      <img src="coin.jpg" alt="coin" height="25" width="25" />
                     </v-btn>
                   </td>
                   <td v-if="user.cart != 0">{{ user.cart }}</td>
@@ -32,6 +33,12 @@
               </tbody>
             </template>
           </v-data-table>
+        </v-card>
+        <v-card v-if="somethingBought" color="warning" height="60" class="mt-4">
+          <v-layout row justify-center align-content-space-between>
+            <v-btn class="mt-2" color="info">Køb</v-btn>
+            <v-btn class="mt-2 ml-6" color="error">Fortryd</v-btn>
+          </v-layout>
         </v-card>
       </v-container>
     </v-flex>
@@ -45,12 +52,13 @@ export default {
   data() {
     return {
       loading: false,
+      somethingBought: false,
       users: [],
       headers: [
         { text: "Navn", value: "name" },
-        { text: "Saldo", value: "balance" },
+        { text: "Saldo", value: "balance" }
       ],
-      width: 5,
+      width: 5
     };
   },
   methods: {
@@ -60,7 +68,9 @@ export default {
           if (change.type == "added") {
             let user = {
               uid: change.doc.id,
-              balance: change.doc.get("balance") ? changes.doc.get("balance") : 0,
+              balance: change.doc.get("balance")
+                ? changes.doc.get("balance")
+                : 0,
               cart: 0,
               name: change.doc.get("name")
             };
@@ -74,6 +84,7 @@ export default {
       this.users.map(user => {
         if (user.uid == userToAddTo.uid) {
           userToAddTo.cart += amount;
+          this.somethingBought = true;
         }
       });
     }
