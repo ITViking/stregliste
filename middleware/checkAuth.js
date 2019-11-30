1796ad6216303e874fcfb754d70d37a82e83f422
ITViking <philip.dein@gmail.com>
2019-11-30 21:11:53 +0100

    remove big middleware router

--- middleware/checkAuth.js
+++
@@ -1,6 +0,0 @@
-
-export default function ({ store, redirect, route }) {
-    if (route.path == "/tap" && !store.state.user.email) return redirect("/");
-    if (route.path == "/" && store.state.user.email) return redirect("/tap");
-    if(route.path == "/root" && !store.state.user.isRoot) return redirect("/tap");
-};
